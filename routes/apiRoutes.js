bdf33d12ac85f8ba80b05a4b273188fab1a47eda
Jennifer Gamez <jenggamez@gmail.com>
2019-12-05 14:44:31 -0800

    added route files and server.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,58 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on notes taken
+// ===============================================================================
+
+var dbJSON = require("../db/db.json");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function(req, res) {
+    res.json(dbJSON);
+  });
+
+  
+  // API POST Requests
+  // Below code handles when a user submits a note and thus submits data to the server.
+  // In each of the below cases, when a user submits note data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/notes", function(req, res) {
+    
+    // Create New Note - takes in JSON input
+    // "Server" will respond to requests and let users know if they have a note or not.
+    // It will do this by sending out the value "true" have a note
+    // req.body is available since we're using the body parsing middleware. req.body hosts is equal to the JSON post sent from the user
+  var newNote = req.body;
+  console.log(newNote);
+  dbJSON.push(newNote);
+
+
+
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    newNote.length = 0;
+   
+
+    res.json({ ok: true });
+  });
+};
